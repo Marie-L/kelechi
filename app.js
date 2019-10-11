ca9cbbf39a3a04b49ab38a15a1e10d5488f84f30
Marie-Louise Annan <ml_annan@hotmail.com>
2019-10-11 12:27:42 +0100

    create multiple endpoints, add content to about.html

--- app.js
+++ app.js
@@ -4,11 +4,36 @@ const express = require('express');
 // call express function and assign it to app var
 const app = express();
 
-// root route
+// call router method on express object and pass to router var
+//const router = express.Router();
+
+// root endpoint
 // call get method on  object, params : location param, require and request objects
 app.get('/', (req, res) =>{
     res.sendFile('index.html', {root:__dirname});
 });
+
+// about endpoint
+app.get('/about', (req,res) => {
+    res.sendFile( __dirname +'/about.html')
+});
+
+// work endpoint
+app.get('/work', (req,res) => {
+    res.sendFile( __dirname +'/work.html')
+});
+
+
+// about endpoint
+app.get('/contact', (req,res) => {
+    res.sendFile( __dirname +'/contact.html')
+});
+
+// about endpoint
+app.get('/blog', (req,res) => {
+    res.sendFile( __dirname +'/blog.html')
+});
+
 
 //call use method on app object to set up static server for js and css files
 app.use('/static', express.static('public'));
