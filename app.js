e10f637108ca3f75e65c721d86f17466d1f23fe6
Marie-Louise Annan <ml_annan@hotmail.com>
2019-10-11 10:38:17 +0100

    create route, static server, add index.hhtml

--- app.js
+++ app.js
@@ -4,5 +4,16 @@ const express = require('express');
 // call express function and assign it to app var
 const app = express();
 
-// call listen method on app var to setup server, pass in port number as param
-app.listen(3001)
+// root route
+// call get method on  object, params : location param, require and request objects
+app.get('/', (req, res) =>{
+    res.sendFile('index.html', {root:__dirname});
+});
+
+//call use method on app object to set up static server for js and css files
+app.use('/static', express.static('public'));
+
+// call listen method on app object to setup dev server, pass in port number as param
+app.listen(3001, () => {
+    console.log ('the app is running on localhost: 3001!')
+});
